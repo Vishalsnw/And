e31b2b0dbdbbab231fd179716a2c54aecf5dd26a
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-07-30 21:20:19 +0530

    Update GoalGuruDatabase.kt

--- app/src/main/java/com/example/goalguru/data/database/GoalGuruDatabase.kt
+++ app/src/main/java/com/example/goalguru/data/database/GoalGuruDatabase.kt
@@ -1,11 +1,39 @@
+/*
+ * Copyright 2023 Your Name
+ * Licensed under Apache-2.0
+ */
+
+package com.example.goalguru.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.goalguru.data.database.dao.GoalDao
+import com.example.goalguru.data.database.dao.TaskDao
+import com.example.goalguru.data.database.dao.UserDao
+import com.example.goalguru.data.database.entities.Goal
+import com.example.goalguru.data.database.entities.Task
+import com.example.goalguru.data.database.entities.User
+
 @Database(
-    entities = [Goal::class, User::class, Task::class],
+    entities = [
+        Goal::class,
+        User::class,
+        Task::class
+    ],
     version = 2,
-    exportSchema = false,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class GoalGuruDatabase : RoomDatabase() {
+
     abstract fun goalDao(): GoalDao
+
     abstract fun userDao(): UserDao
+
     abstract fun taskDao(): TaskDao
+
+    companion object {
+        const val DATABASE_NAME = "goal_guru_db"
+    }
 }
