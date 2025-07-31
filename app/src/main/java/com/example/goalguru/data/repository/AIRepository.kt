6a0d5b1d1e4192c3fa71e5eb4ea33f4fd72f497c
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-07-31 09:21:56 +0530

    Update AIRepository.kt

--- app/src/main/java/com/example/goalguru/data/repository/AIRepository.kt
+++ app/src/main/java/com/example/goalguru/data/repository/AIRepository.kt
@@ -7,6 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 import javax.inject.Singleton
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Singleton
 class AIRepository @Inject constructor() {
