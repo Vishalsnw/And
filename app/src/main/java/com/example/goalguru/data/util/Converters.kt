d53047541edd04221cd6b7f8412ee5dea5474e39
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-07-31 08:38:34 +0530

    Create Converters.kt

---
+++ app/src/main/java/com/example/goalguru/data/util/Converters.kt
@@ -0,0 +1,29 @@
+package com.example.goalguru.data.util
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.util.Date
+
+class Converters {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? = value?.let { Date(it) }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? = date?.time
+
+    @TypeConverter
+    fun fromStringList(value: String?): List<String> {
+        if (value.isNullOrEmpty()) return emptyList()
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun listToString(list: List<String>?): String {
+        return gson.toJson(list ?: emptyList())
+    }
+}
