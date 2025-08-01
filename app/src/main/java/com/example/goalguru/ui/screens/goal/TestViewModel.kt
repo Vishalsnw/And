a0e0666e11ad0931ecede15689d956cfe466dcd3
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-08-01 10:12:26 +0530

    Create TestViewModel.kt

---
+++ app/src/main/java/com/example/goalguru/ui/screens/goal/TestViewModel.kt
@@ -0,0 +1,9 @@
+// File: TestViewModel.kt (put it anywhere)
+package com.example.goalguru
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TestViewModel @Inject constructor() : ViewModel()
